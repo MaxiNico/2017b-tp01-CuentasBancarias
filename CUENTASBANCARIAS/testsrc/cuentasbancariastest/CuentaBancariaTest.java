a5513be8170db50df86d218535f33af51d7039b5
Maximiliano Lima <maxi.lima94@gmail.com>
2017-09-06 23:45:21 -0300

    ROJO: Que se pueda crear cuenta con salgo inicial

--- CUENTASBANCARIAS/testsrc/cuentasbancariastest/CuentaBancariaTest.java
+++ CUENTASBANCARIAS/testsrc/cuentasbancariastest/CuentaBancariaTest.java
@@ -13,9 +13,11 @@ public class CuentaBancariaTest {
 	public void testGetSaldo() {
 		
 		CuentaBancaria cuenta1 = new CuentaBancaria();
+		CuentaBancaria cuenta2 = new CuentaBancaria(200);
 		
 		assertEquals(0, cuenta1.getSaldo(), 0);  //Se usa el Delta por la impresicion cuando se comparan dos double, o algo asi
 		
+		assertEquals(200, cuenta2.getSaldo(), 0);
 		
 		
 	}
