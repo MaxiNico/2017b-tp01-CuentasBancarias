3090436ce47c5767819b74e3f0e40dc02481f844
Maximiliano Lima <maxi.lima94@gmail.com>
2017-09-06 23:56:45 -0300

    ROJO: Quiero poder sumarle saldo a una cuenta

--- CUENTASBANCARIAS/testsrc/cuentasbancariastest/CuentaBancariaTest.java
+++ CUENTASBANCARIAS/testsrc/cuentasbancariastest/CuentaBancariaTest.java
@@ -18,8 +18,16 @@ public class CuentaBancariaTest {
 		assertEquals(0, cuenta1.getSaldo(), 0);  //Se usa el Delta por la impresicion cuando se comparan dos double, o algo asi
 		
 		assertEquals(200, cuenta2.getSaldo(), 0);
+	}
+	
+	@Test
+	public void testSumarSaldo(){
 		
+		CuentaBancaria cuenta1 = new CuentaBancaria();
 		
+		cuenta1.sumarSaldo(3000);
+		
+		assertEquals(3000, cuenta1.getSaldo(), 0);
 	}
 
 }
