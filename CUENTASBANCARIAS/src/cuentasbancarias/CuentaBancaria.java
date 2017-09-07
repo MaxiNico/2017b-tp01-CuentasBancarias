ebf15d9c70e1f46bca45225ceab79a74f8cf0c45
Maximiliano Lima <maxi.lima94@gmail.com>
2017-09-06 23:42:18 -0300

    VERDE: tengo saldo y la cuenta se inicia en 0

--- CUENTASBANCARIAS/src/cuentasbancarias/CuentaBancaria.java
+++ CUENTASBANCARIAS/src/cuentasbancarias/CuentaBancaria.java
@@ -2,6 +2,16 @@ package cuentasbancarias;
 
 public class CuentaBancaria {
 	
+	private double saldo;
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+	
 	
 
 }
